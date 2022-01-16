f25a3424b8508a1258f4a0d703a2f0f086dbde49
Heewon Lee <ncross42@gmail.com>
2022-01-17 01:28:48 +0900

    add insert by sqlx

--- go.mod
+++ go.mod
@@ -2,12 +2,19 @@ module github.com/ncross42/ssfo_excel
 
 go 1.17
 
-require github.com/xuri/excelize/v2 v2.5.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/xuri/excelize/v2 v2.5.0
+)
 
 require (
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.3 // indirect
 	github.com/richardlehane/msoleps v1.0.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/xuri/efp v0.0.0-20210322160811-ab561f5b45e3 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ncross42/ssfo_excel/utils"
 
 	"github.com/xuri/excelize/v2"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Visitor struct {
+	CarNo  string `db:"car_no"`
+	InDate string `db:"in_date"`
+	InTime string `db:"in_time"`
+	Door   string `db:"door"`
+}
 
 func GetHeaderFiltered(rows *excelize.Rows) []int {
 	var header_all []string
@@ -62,6 +74,62 @@ func GetData(f *excelize.File) [][]string {
 	return data
 }
 
+func GetHeaderFiltered2(rows *excelize.Rows) map[string]int {
+	var header_all []string
+	if !rows.Next() {
+		fmt.Println("Rows tailed to iterate in GetHeaderFiltered")
+		return nil
+	}
+	row, err := rows.Columns()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	header_all = append(header_all, row...)
+
+	fmt.Println(len(header_all), header_all)
+	var header_filtered map[string]int = make(map[string]int)
+	for row_no, val := range header_all {
+		switch val {
+		case "입차일자", "입차시각", "입차차량번호", "입차기기", "동", "호":
+			header_filtered[val] = row_no
+		}
+	}
+	fmt.Println(header_filtered)
+	return header_filtered
+}
+
+func GetData2(f *excelize.File) []Visitor {
+	// Rows returns a rows iterator, used for streaming reading data for a worksheet with a large data
+	rows, err := f.Rows("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	var col_index map[string]int = GetHeaderFiltered2(rows)
+	var data []Visitor // = make([]Visitor, rows.TotalRows()-1)
+	for rows.Next() {
+		row, err := rows.Columns()
+		if err != nil {
+			fmt.Println(err)
+		}
+		// for _, col := range col_index {
+		// 	data[row_no] = append(data[row_no], row[col])
+		// }
+		data = append(data, Visitor{
+			CarNo:  row[col_index["입차차량번호"]],
+			InDate: row[col_index["입차일자"]],
+			InTime: row[col_index["입차시각"]],
+			Door:   row[col_index["입차기기"]],
+		})
+		// fmt.Println(data[row_no])
+	}
+	if err = rows.Close(); err != nil {
+		fmt.Println(err)
+	}
+	return data
+}
+
 func main() {
 	utils.PrintMemUsage()
 
@@ -78,9 +146,40 @@ func main() {
 	}()
 
 	// Get all the rows in the Sheet1.
-	var data [][]string = GetData(f)
+	// var data [][]string = GetData(f)
+	var data []Visitor = GetData2(f)
 	fmt.Println(len(data))
-	// pp.Print(data)
+	fmt.Println(data)
+
+	in_cnt := InsertData(data)
+	fmt.Println(in_cnt)
 
 	utils.PrintMemUsage()
+}
+
+func InsertData(visitors []Visitor) int64 {
+
+	// use sqlx.Open() for sql.Open() semantics
+	db, err := sqlx.Connect("mysql", "root:itsme1@(localhost:3306)/fore")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		// Close the spreadsheet.
+		if err := db.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	r, err := db.NamedExec(`INSERT INTO visitor (car_no, in_date, in_time, door)
+		VALUES (:car_no, :in_date, :in_time, :door)`, visitors)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	in_cnt, err := r.RowsAffected()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return in_cnt
 }
